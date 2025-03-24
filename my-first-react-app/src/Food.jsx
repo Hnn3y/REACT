c6177e126a22f504a55f0dc33a43ae74d6ca26ae
Enijeshiku Eniola <eenijeshiku@gmail.com>
2025-03-24 23:07:40 +0100

    BUILD: React files added

---
+++ my-first-react-app/src/Food.jsx
@@ -0,0 +1,13 @@
+function Food() {
+    return (
+    <div>
+        <ol>
+            <li>Rice</li>
+            <li>Beans</li>
+            <li>Cucumber</li> 
+        </ol>
+    </div>
+    );
+} 
+
+export default Food;
---
+++ my-first-react-app/src/Greeting.jsx
@@ -0,0 +1,7 @@
+function Greeting () {
+    return <h1>&quot; My name is Henney &quot;</h1>
+
+    
+} 
+
+export default Greeting;
