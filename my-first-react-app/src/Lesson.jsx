64d8222b4a1e6a7746cc31487ea52f591720fe8a
Enijeshiku Eniola <eenijeshiku@gmail.com>
2025-03-25 01:45:19 +0100

    BUILD: Added lesson.jsx

--- my-first-react-app/src/Lesson.jsx
+++ my-first-react-app/src/Lesson.jsx
@@ -1,23 +1,22 @@
 function Lesson (){
-    return(
+    const animals = ["Lion","Cow", "Snake", "Chicken"]
+
+    return( 
         <div className="container"> 
         <svg>
           <circle cx="20" cy="75" r="30" stroke="blue" strokeWidth="2"  />
         </svg>
         <form>
-          <input type="text"/> 
-          <h1>
+          <input type="text"/>
+        </form>
+        <h1>
             Animals:
           </h1> 
-
           <ul>
-            <li>Lion</li>
-            <li>Cow</li>
-            <li>Snake</li>
-            <li>Lizard</li>
-            <li>Chicken</li>
+            {animals.map((animal) => {
+                return <li key={animal}>{animal}</li>;
+             })}
           </ul>
-        </form>
         </div>
     );
     
