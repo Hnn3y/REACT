5d5eccb55a2ca8aefe348506de4e46afe04823e7
Enijeshiku Eniola <eenijeshiku@gmail.com>
2025-03-26 17:55:25 +0100

    BUILD: Changes made to App

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -4,16 +4,29 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Food from './Food'
 import Greeting from './Greeting'
-import Video from './Video'
+
+
+function Button(props) {
+  const buttonStyle ={
+    color: props.color,
+    fontSize: props.fontSize + 'px'
+  };
+
+  return(
+    <button style={buttonStyle}>{props.text}</button>
+  );
+}
 
 function App() {
  
   return (
     <div>
-      <div>
+     
       <h2>My Favorite foods</h2>
-      <Food/>
-      </div>
+      <Food text="Click"/>
+      <Button text="Click Me" color="Bliue" fontSize={12}/>
+      <Button text="Don't Click Me!" color="red" fontSize={20} />
+      <Button text="Click Me!" color="blue" fontSize={20} />
     </div>
 
   )
--- my-first-react-app/src/Video.jsx
+++ my-first-react-app/src/Video.jsx
@@ -1,4 +1,10 @@
-function Video({ video }) {
+
+
+
+
+
+function Video( video ) {
+
     return(
         <div>
         <Thumbnail video={video} />
