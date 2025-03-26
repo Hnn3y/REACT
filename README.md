88d4bbb467361ad44410e3252e795d3451d341e8
Enijeshiku Eniola <eenijeshiku@gmail.com>
2025-03-26 18:22:00 +0100

    BUILD

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # REACT
+
+State definition with useState commonly follows this pattern:
+
+const [stateValue, setStateValue] = useState(initialValue);
--- my-first-react-app/src/months.jsx
+++ my-first-react-app/src/months.jsx
@@ -1,12 +1,34 @@
-const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+import { useState } from "react";
+import "./App.css";
 
-function List() {
-    return(
-        <ul>
-            {}
-            {months.map((month,index) => (<li key={index}>{month}</li>))}
-        </ul>
-    );
-} 
+const COLORS = ["pink", "green", "blue", "yellow", "purple"];
 
-export default List;
+function App() {
+  const [backgroundColor, setBackgroundColor] = useState(COLORS[2]);
+
+  const onButtonClick = (color) => () => {
+    setBackgroundColor(color);
+  };
+
+  return (
+    <div
+      className="App"
+      style={{
+        backgroundColor,
+      }}
+    >
+      {COLORS.map((color) => (
+        <button
+          type="button"
+          key={color}
+          onClick={onButtonClick(color)}
+          className={backgroundColor === color ? "selected" : ""}
+        >
+          {color}
+        </button>
+      ))}
+    </div>
+  );
+}
+
+export default App;
